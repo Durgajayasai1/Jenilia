b5b09eb49f5fb1ecd425678f22d13e2c8f00d688
Durgajayasai1 <durgajayasai08072004@gmail.com>
2024-03-12 03:06:43 +0530

    qualification, skills, portfolio done

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,16 +10,67 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'), tabcontent = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab=>{
+    tab.addEventListener("click",()=>{
+        const target=document.querySelector(tab.dataset.target);
+
+        tabcontent.forEach(tabcontents=>{
+            tabcontents.classList.remove('skills__active');
+        })
+
+        target.classList.add('skills__active');
+
+        tabs.forEach(tab=>{
+            tab.classList.remove('skills__active');
+        })
+
+        tab.classList.add('skills__active');
+    })
+})
 
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 
 /*===== Link Active Work =====*/
+const linkwork = document.querySelectorAll('.work__item');
 
+function activework(){
+    linkwork.forEach(l=>l.classList.remove('active__work'));
+    this.classList.add('active__work');
+}
+
+linkwork.forEach(l=>l.addEventListener("click", activework));
 
 /*===== Work Popup =====*/
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("work__button")){
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+});
 
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click",togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
 
 /*=============== SERVICES MODAL ===============*/
 
